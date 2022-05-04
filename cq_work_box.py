10cc5ad223212285efa3488fbd221203775350e7
Connor Hull <connor@hull.is>
2022-05-04 15:21:54 -0500

    Add second button hole for lid, small tweaks to optimize print

--- cq_work_box.py
+++ cq_work_box.py
@@ -4,7 +4,7 @@ import cadquery as cq
 #------------------
 
 length = 90.0
-width = 65
+width = 53
 height = 25
 
 shell_thickness = 2
@@ -13,16 +13,16 @@ shell_thickness = 2
 # adjust for 3d printing or if you want to add a gasket
 fitment_epsilon = 0.1
 
-button_diam = 12.5
+button_diam = 12.7
 button_offset = 18
 
-screw_post_diam = 4
-screw_hole_size = 2.5 #M3 Bolt
+screw_post_diam = 6
+screw_hole_diam = 2.5 #M3 Bolt
 
 # wiring port dimensions
 wire_port_offset = (16, 4)
 wire_port_width = 5
-wire_port_height = 2
+wire_port_height = 3
 
 # END PARAMS
 #-----------------------
@@ -32,8 +32,8 @@ lid_width = width + (shell_thickness * 2)
 lid_length = length + (shell_thickness * 2)
 
 screw_post_rect = (
-    width - (screw_post_diam / 2 + shell_thickness), 
-    length - (screw_post_diam / 2 + shell_thickness)
+    width - (screw_post_diam / 2), 
+    length - (screw_post_diam / 2)
 )
 
 
@@ -47,9 +47,9 @@ base = cq.Workplane("front")\
 # posts for screwing lid
 base = base.faces("<Z").workplane(centerOption="CenterOfMass", offset=-shell_thickness)\
     .rect(screw_post_rect[0], screw_post_rect[1], forConstruction=True)\
-    .vertices().circle(screw_post_diam).extrude(-height)\
+    .vertices().circle(screw_post_diam / 2).extrude(-height)\
     .rect(screw_post_rect[0], screw_post_rect[1], forConstruction=True)\
-    .vertices().circle(screw_hole_size).cutBlind(-height)\
+    .vertices().circle(screw_hole_diam / 2).cutBlind(-height)\
 
 # Thru holes for wiring
 base = base.faces(">Y").workplane(centerOption="CenterOfMass")\
@@ -61,20 +61,20 @@ base = base.faces(">Y").workplane(centerOption="CenterOfMass")\
     .rect(wire_port_width, wire_port_height).cutBlind(-shell_thickness)
 
 lid = base.faces(">Z").workplane(centerOption="CenterOfMass")\
-    .box(lid_width, lid_length, 4, combine=False)\
+    .box(lid_width, lid_length, 4.5, combine=False)\
     .edges("|Z").fillet(shell_thickness)\
     .faces("<Z").tag("rim")\
     .faces("<Z").workplane().box(
         width - (fitment_epsilon * 2), 
         length - (fitment_epsilon * 2), 
-        shell_thickness / 2
+        shell_thickness
     )\
     .faces("<Z").workplane()\
     .rect(screw_post_rect[0], screw_post_rect[1], forConstruction=True)\
-    .vertices().circle(screw_post_diam).cutBlind(-shell_thickness / 2)\
+    .vertices().circle(screw_post_diam / 2).cutBlind(-shell_thickness / 2)\
     .faces(">Z").workplane()\
     .rect(screw_post_rect[0], screw_post_rect[1], forConstruction=True)\
-    .vertices().cboreHole(screw_hole_size, 6.5, 3.1) # M3 socket head
+    .vertices().cboreHole(screw_hole_diam, 6.5, 3.1) # M3 socket head
     
 # Lid top detail
 lid = lid.faces(">Z").workplane()\
@@ -84,6 +84,15 @@ lid = lid.faces(">Z").workplane()\
         forConstruction=True
     )\
     .vertices(">XY")\
+    .hole(button_diam)
+
+lid = lid.faces(">Z").workplane()\
+    .rect(
+        lid_width - (button_offset * 2), 
+        lid_length - (button_offset * 2), 
+        forConstruction=True
+    )\
+    .vertices("<XY")\
     .hole(button_diam) 
 
 # Render the solid
@@ -101,7 +110,7 @@ part = (
 )
 
 #Editor only
-#show_object(part)
+show_object(lid)
 
 cq.exporters.export(base, "workBox.stl")
 cq.exporters.export(lid, "workBoxLid.stl")
